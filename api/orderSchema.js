98956716a8015263bd0ae76c7dca19a7ce365508
Ashraf 0011 <md_ashraful@icloud.com>
2023-01-19 10:47:19 +0930

    order details are verified and collected

--- api/orderSchema.js
+++ api/orderSchema.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema
 const OrderSCHEMA = new Schema({
-    orderID: { type: Number },
-    billingInfo: { type: Array },
-    shippingInfo: { type: Array },
-    orderedItemInfo: { type: Array },
-    paymentInfo: { type: Array }
+    Item: { type: Object },
+    userName: { type: String },
+    userEmail: { type: String },
+    userPhone: { type: String },
+    userAddress: { type: String },
+    userPostCode: { type: String },
+    userCity: { type: String },
+    userCountry: { type: String },
+    paymentMethod: { type: String },
+    e_Number: { type: String },
+    e_Pin: { type: String },
+    invoiceAmount: { type: String }
 }, { collection: "OrderInformation" });
 const mdl = mongoose.model("ORDER_SCHEMA", OrderSCHEMA);
 module.exports = mdl
