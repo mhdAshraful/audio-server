21f0731c1ffdf69f3f3f806222a8d8548d221763
@mhdAshraful <mycdu.edu@icloud.com>
2024-07-25 23:11:58 +1000

    monogoDB T&C caused issues

--- index.js
+++ index.js
@@ -11,33 +11,35 @@ const port = 5555;
 const app = express();
 
 // has to be before eveything else
-app.use('/', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "*")
-    next();
-})
-
+app.use("/", (req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	next();
+});
+// currently running in local after accepting mongoDB T&C
 app.use(
-    cors({
-        origin: "*",
-        methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-        preflightContinue: true,
-        credentials: true,
-    })
+	cors({
+		origin: "*",
+		methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+		preflightContinue: true,
+		credentials: true,
+	})
 );
 
 app.use(json());
 
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dbcluster.nlm3zmb.mongodb.net/${process.env.COLLECTION_NAME}?retryWrites=true&w=majority`
+	)
+	.then(() => {
+		console.log("database connected");
+	})
+	.catch((e) => {
+		console.log("Error detected:", e);
+	});
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dbcluster.nlm3zmb.mongodb.net/${process.env.COLLECTION_NAME}?retryWrites=true&w=majority`)
-    .then(() => {
-        console.log("database connected");
-    })
-    .catch((e) => {
-        console.log("Error detected:", e);
-    })
-
-app.use('/', ServerRouter);
+app.use("/", ServerRouter);
 
 app.listen(port, () => {
-    console.log(`server running at ${port}`);
+	console.log(`server running at ${port}`);
 });
