a9a57ef72bf8e8175adea2c63b09edd98d0b8d26
mhdAshraful <md_ashraful@icloud.com>
2025-04-18 23:57:25 +0600

    latest packages

--- api/productSchema.js
+++ api/productSchema.js
@@ -1,73 +1,76 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const PSCHEMA = new Schema({
-    id: { type: Number },
-    category: { type: String },
-    categoryimage: {
-        type: Object,
-        desktop: { type: String },
-        mobile: { type: String },
-        tablet: { type: String }
-    },
-    description: { type: String },
-    features: { type: String },
-    gallery: {
-        type: Object,
-        first: {
-            type: Object,
-            desktop: { type: String },
-            mobile: { type: String },
-            tablet: { type: String }
-        },
-        second: {
-            type: Object,
-            desktop: { type: String },
-            mobile: { type: String },
-            tablet: { type: String }
-        },
-        third: {
-            type: Object,
-            desktop: { type: String },
-            mobile: { type: String },
-            tablet: { type: String }
-        }
-    },
-    image: {
-        type: Object,
-        desktop: { type: String },
-        mobile: { type: String },
-        tablet: { type: String }
-    },
-    includes: {
-        type: Array,
-        document: {
-            type: Object,
-            item: { type: String },
-            quantity: { type: Number },
-        },
-    },
-    name: { type: String },
-    new: { type: Boolean },
-    others: {
-        type: Array,
-        Document: {
-            type: Object,
-            image: {
-                type: Object,
-                desktop: { type: String },
-                mobile: { type: String },
-                tablet: { type: String }
-            },
-            name: { type: String },
-            slug: { type: String }
-        }
-    },
-    price: {
-        type: Number
-    },
-    slug: { type: String }
-}, { collection: "AllProducts" });
+const PSCHEMA = new Schema(
+	{
+		id: { type: Number },
+		category: { type: String },
+		categoryimage: {
+			type: Object,
+			desktop: { type: String },
+			mobile: { type: String },
+			tablet: { type: String },
+		},
+		description: { type: String },
+		features: { type: String },
+		gallery: {
+			type: Object,
+			first: {
+				type: Object,
+				desktop: { type: String },
+				mobile: { type: String },
+				tablet: { type: String },
+			},
+			second: {
+				type: Object,
+				desktop: { type: String },
+				mobile: { type: String },
+				tablet: { type: String },
+			},
+			third: {
+				type: Object,
+				desktop: { type: String },
+				mobile: { type: String },
+				tablet: { type: String },
+			},
+		},
+		image: {
+			type: Object,
+			desktop: { type: String },
+			mobile: { type: String },
+			tablet: { type: String },
+		},
+		includes: {
+			type: Array,
+			document: {
+				type: Object,
+				item: { type: String },
+				quantity: { type: Number },
+			},
+		},
+		name: { type: String },
+		new: { type: Boolean },
+		others: {
+			type: Array,
+			Document: {
+				type: Object,
+				image: {
+					type: Object,
+					desktop: { type: String },
+					mobile: { type: String },
+					tablet: { type: String },
+				},
+				name: { type: String },
+				slug: { type: String },
+			},
+		},
+		price: {
+			type: Number,
+		},
+		slug: { type: String },
+	},
+	{ collection: "AllProducts" }
+);
 
 const mdl = mongoose.model("PRODUCT_SCHEMA", PSCHEMA);
 module.exports = mdl;
