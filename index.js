c34d5663b8af158408b3f309cc9d20b6fbf857ea
mhdAshraful <md_ashraful@icloud.com>
2025-04-20 18:00:11 +0600

    cors

--- index.js
+++ index.js
@@ -9,11 +9,24 @@ dotenv.config();
 const app = express();
 const port = 5555;
 
-app.use(cors());
-app.use((req, res, next) => {
-	console.log("📨 Request Headers:", req.headers);
-	next();
-});
+const whitelist = ["https://audiophile-green-alpha.vercel.app"];
+const corsOptions = {
+	origin: function (origin, callback) {
+		if (!origin || whitelist.includes(origin)) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	credentials: true,
+	methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+	allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+	exposedHeaders: ["Content-Length"],
+	optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
 app.use(json());
 
 // ✅ Connect MongoDB
